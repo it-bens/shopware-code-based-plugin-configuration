77c51887601c6ff2a3c608a0b44d654fe2baa3aa
Martin Bens <martin.bens@it-bens.de>
2024-07-28 01:27:50 +0200

    Tests for `ConfigurationCardConfigSaver` and `ConfigurationCardConfigSaverPass` added.

--- tests/Unit/ConfigurationCardConfigReader/ConfigurationCardProviderProviderTest.php
+++ tests/Unit/ConfigurationCardConfigReader/ConfigurationCardProviderProviderTest.php
@@ -20,6 +20,60 @@ final class ConfigurationCardProviderProviderTest extends TestCase
         yield [[$configurationCardProvider]];
     }
 
+    public static function getBundleClassesProvider(): \Generator
+    {
+        $configurationCardProviderProvider = new ConfigurationCardProviderProvider([]);
+        yield 'without configuration card providers' => [$configurationCardProviderProvider, []];
+
+        $configurationCardProvider1 = self::createStub(ConfigurationCardProvider::class);
+        $configurationCardProvider1->method('getBundleClasses')
+            ->willReturn(['BundleClass1', 'BundleClass2']);
+        $configurationCardProvider2 = self::createStub(ConfigurationCardProvider::class);
+        $configurationCardProvider2->method('getBundleClasses')
+            ->willReturn(['BundleClass3', 'BundleClass4']);
+
+        $configurationCardProviderProvider = new ConfigurationCardProviderProvider([
+            $configurationCardProvider1,
+            $configurationCardProvider2,
+        ]);
+        yield 'with two configuration card providers with different bundle classes' => [
+            $configurationCardProviderProvider,
+            ['BundleClass1', 'BundleClass2', 'BundleClass3', 'BundleClass4'],
+        ];
+
+        $configurationCardProvider3 = self::createStub(ConfigurationCardProvider::class);
+        $configurationCardProvider3->method('getBundleClasses')
+            ->willReturn(['BundleClass1', 'BundleClass2']);
+        $configurationCardProvider4 = self::createStub(ConfigurationCardProvider::class);
+        $configurationCardProvider4->method('getBundleClasses')
+            ->willReturn(['BundleClass2', 'BundleClass3']);
+
+        $configurationCardProviderProvider = new ConfigurationCardProviderProvider([
+            $configurationCardProvider3,
+            $configurationCardProvider4,
+        ]);
+        yield 'with two configuration card providers with partly overlapping bundle classes' => [
+            $configurationCardProviderProvider,
+            ['BundleClass1', 'BundleClass2', 'BundleClass3'],
+        ];
+
+        $configurationCardProvider5 = self::createStub(ConfigurationCardProvider::class);
+        $configurationCardProvider5->method('getBundleClasses')
+            ->willReturn(['BundleClass1', 'BundleClass2']);
+        $configurationCardProvider6 = self::createStub(ConfigurationCardProvider::class);
+        $configurationCardProvider6->method('getBundleClasses')
+            ->willReturn(['BundleClass1', 'BundleClass2']);
+
+        $configurationCardProviderProvider = new ConfigurationCardProviderProvider([
+            $configurationCardProvider5,
+            $configurationCardProvider6,
+        ]);
+        yield 'with two configuration card providers with overlapping bundle classes' => [
+            $configurationCardProviderProvider,
+            ['BundleClass1', 'BundleClass2'],
+        ];
+    }
+
     public static function getConfigurationCardProvidersProvider(): \Generator
     {
         $configurationCardProviderProvider = new ConfigurationCardProviderProvider([]);
@@ -50,6 +104,18 @@ final class ConfigurationCardProviderProviderTest extends TestCase
     {
         $configurationCardProviderProvider = new ConfigurationCardProviderProvider($configurationCardProviders);
         $this->assertInstanceOf(ConfigurationCardProviderProvider::class, $configurationCardProviderProvider);
+    }
+
+    /**
+     * @param string[] $expectedBundleClasses
+     */
+    #[DataProvider('getBundleClassesProvider')]
+    public function testGetBundleClasses(
+        ConfigurationCardProviderProvider $configurationCardProviderProvider,
+        array $expectedBundleClasses
+    ): void {
+        $bundleClasses = $configurationCardProviderProvider->getBundleClasses();
+        $this->assertSame($expectedBundleClasses, $bundleClasses);
     }
 
     /**
