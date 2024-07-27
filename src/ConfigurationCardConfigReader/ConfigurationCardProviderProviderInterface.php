fc59683c85a50fc25898ecc965a30125934f00f9
Martin Bens <martin.bens@it-bens.de>
2024-07-27 22:53:55 +0200

    Method added to ConfigurationCardProviderProviderInterface to get all bundle classes that use "this package".

--- src/ConfigurationCardConfigReader/ConfigurationCardProviderProviderInterface.php
+++ src/ConfigurationCardConfigReader/ConfigurationCardProviderProviderInterface.php
@@ -5,9 +5,15 @@ declare(strict_types=1);
 namespace ITB\ShopwareCodeBasedPluginConfiguration\ConfigurationCardConfigReader;
 
 use ITB\ShopwareCodeBasedPluginConfiguration\ConfigurationCardProvider;
+use Shopware\Core\Framework\Bundle;
 
 interface ConfigurationCardProviderProviderInterface
 {
+    /**
+     * @return class-string<Bundle>[]
+     */
+    public function getBundleClasses(): array;
+
     /**
      * @return iterable<ConfigurationCardProvider>
      */
